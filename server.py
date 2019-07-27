4a6eac7d8622b12062abc5280cc9eb40c1456bf2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-26 17:15:32 -0700

    WIP - importing eventbrite API into script

--- server.py
+++ server.py
@@ -45,6 +45,10 @@ def find_afterparties():
         #   the form data.
         # - (Make sure to save the JSON data from the response to the data
         #   variable so that it can display on the page as well.)
+        payload = {'token' : EVENTBRITE_TOKEN, "q" : query , 
+                    "location.address" : location, "location.within" : distance}
+        url = 'https://www.eventbriteapi.com/v3/events/search/'
+        response = requests.get(url, params=payload)
 
         data = {'This': ['Some', 'mock', 'JSON']}
         events = []
